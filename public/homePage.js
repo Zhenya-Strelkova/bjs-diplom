ae4cade5a4f7c54fe8062abdb69839780b3bc5e8
Zhenya-Strelkova <zhenya.strelkova@mail.ru>
2022-02-02 18:27:10 +0300

    refactor using decorators

--- public/homePage.js
+++ public/homePage.js
@@ -1,95 +1,107 @@
 "use strict";
 
+//----------------ДЕКОРАТОРЫ----------------------------------------------
+//Декоратор функции-callback - проверки успешности запроса
+function decoratorCallbackSuccess(funcSuccess, funcError) {
+    function wrapper(response) {
+        if (response.success) {
+            funcSuccess(response);
+        } else {
+            funcError(response);
+        }
+    }
+    return wrapper;
+}
+
+//Декоратор вызова к серверу с использованием данных
+function decoratorApiConnectWithData(methodName, funcCallback) {
+    function wrapper(data) {
+        ApiConnector[methodName](data, funcCallback);
+    }
+    return wrapper;
+}
+
+//Декоратор вызова к серверу без использования данных
+function decoratorApiConnectWithoutData(methodName, funcCallback) {
+    function wrapper() {
+        ApiConnector[methodName](funcCallback);
+    }
+    return wrapper;
+}
+
+function decoratorSetMessage(func, message) {
+    function wrapper(response) {
+        if (func) func(response);
+        if (response.success) {
+            favoritesWidget.setMessage(response.success, message);
+        } else favoritesWidget.setMessage(response.success, response.error);
+    }
+    return wrapper;
+}
+
+//------------------ПОДГРУЗКА НАЧАЛЬНЫХ ДАННЫХ СТРАНИЦЫ------------------------------------
+//действия в случае успешного выхода
+function reloadPage() {
+    location.reload();
+}
+
 //Кнопка выхода из личного кабинета
 let logoutButton = new LogoutButton();
-logoutButton.action = () => {
-    ApiConnector.logout((response) => {
-        if (response.success) {
-            location.reload();
-        }
-    });
-};
+logoutButton.action =
+    decoratorApiConnectWithoutData('logout', decoratorCallbackSuccess(reloadPage));
 
 //Подгрузка информации о текущем пользователе
-ApiConnector.current((response) => {
-    if (response.success) {
-        ProfileWidget.showProfile(response.data);
-    }
-});
+function showProfileSuccess(response) {
+    ProfileWidget.showProfile(response.data);
+}
+decoratorApiConnectWithoutData('current', decoratorCallbackSuccess(showProfileSuccess))();
 
 //Подгрузка курса валют
 let ratesBoard = new RatesBoard();
-function ratesBoardCall() {
-    ApiConnector.getStocks((response) => {
-        if (response.success) {
-            ratesBoard.clearTable();
-            ratesBoard.fillTable(response.data);
-        }
-    });
+function getStocksSuccess(response) {
+    ratesBoard.clearTable();
+    ratesBoard.fillTable(response.data);
 }
+const ratesBoardCall = decoratorApiConnectWithoutData('getStocks', decoratorCallbackSuccess(getStocksSuccess));
 ratesBoardCall();
 setInterval(ratesBoardCall, 60000);
 
-//Работа с избранным
+//---------------------РАБОТА С ИЗБРАННЫМ-------------------------------------
 let favoritesWidget = new FavoritesWidget();
-//Подгрузка списка избранных пользователей
-ApiConnector.getFavorites((response) => {
-    if (response.success) {
-        favoritesWidget.clearTable();
-        favoritesWidget.fillTable(response.data);
-        moneyManager.updateUsersList(response.data);
-    }
-});
-//Добавление пользователя в список избранных
-favoritesWidget.addUserCallback = (user) => {
-    ApiConnector.addUserToFavorites(user, (response) => {
-        if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(response.success, 'Пользователь добавлен');
-        } else favoritesWidget.setMessage(response.success, response.error);
-    });
-};
-//Удаление пользователя из списка избранных
-favoritesWidget.removeUserCallback = (user) => {
-    ApiConnector.removeUserFromFavorites(user, (response) => {
-        if (response.success) {
-            favoritesWidget.clearTable();
-            favoritesWidget.fillTable(response.data);
-            moneyManager.updateUsersList(response.data);
-            favoritesWidget.setMessage(response.success, 'Пользователь удален');
-        } else favoritesWidget.setMessage(response.success, response.error);
-    });
-};
 
-//Операции с деньгами
+//Подгрузка списка избранных пользователей
+function getFavoritesSuccess(response) {
+    favoritesWidget.clearTable();
+    favoritesWidget.fillTable(response.data);
+    moneyManager.updateUsersList(response.data);
+}
+decoratorApiConnectWithoutData('getFavorites', decoratorCallbackSuccess(getFavoritesSuccess))();
+
+//Добавление пользователя в список избранных
+favoritesWidget.addUserCallback =
+    decoratorApiConnectWithData('addUserToFavorites',
+        decoratorCallbackSuccess(decoratorSetMessage(getFavoritesSuccess, 'Пользователь добавлен'), decoratorSetMessage()));
+
+//Удаление пользователя из списка избранных
+favoritesWidget.removeUserCallback =
+    decoratorApiConnectWithData('removeUserFromFavorites',
+        decoratorCallbackSuccess(decoratorSetMessage(getFavoritesSuccess, 'Пользователь удален'), decoratorSetMessage()));
+
+//--------------------ОПЕРАЦИИ С ДЕНЬГАМИ-------------------------------
 let moneyManager = new MoneyManager();
+
 //Пополнение своего баланса
-moneyManager.addMoneyCallback = (data) => {
-    ApiConnector.addMoney(data, (response) => {
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            favoritesWidget.setMessage(response.success, 'Баланс пополнен');
-        } else favoritesWidget.setMessage(response.success, response.error);
-    });
-};
+moneyManager.addMoneyCallback =
+    decoratorApiConnectWithData('addMoney',
+        decoratorCallbackSuccess(decoratorSetMessage(showProfileSuccess, 'Баланс пополнен'), decoratorSetMessage()));
+
 //Конвертирование валюты
-moneyManager.conversionMoneyCallback = (data) => {
-    ApiConnector.convertMoney(data, (response) => {
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            favoritesWidget.setMessage(response.success, 'Конвертирование валюты выполнено');
-        } else favoritesWidget.setMessage(response.success, response.error);
-    });
-};
+moneyManager.conversionMoneyCallback =
+    decoratorApiConnectWithData('convertMoney',
+        decoratorCallbackSuccess(decoratorSetMessage(showProfileSuccess, 'Конвертирование валюты выполнено'),
+            decoratorSetMessage()));
+
 //Перевод валюты
-moneyManager.sendMoneyCallback = (data) => {
-    ApiConnector.transferMoney(data, (response) => {
-        console.log(response);
-        if (response.success) {
-            ProfileWidget.showProfile(response.data);
-            favoritesWidget.setMessage(response.success, 'Перевод завершен');
-        } else favoritesWidget.setMessage(response.success, response.error);
-    });
-};
+moneyManager.sendMoneyCallback =
+    decoratorApiConnectWithData('transferMoney',
+        decoratorCallbackSuccess(decoratorSetMessage(showProfileSuccess, 'Перевод завершен'), decoratorSetMessage()));
--- public/loginPage.js
+++ public/loginPage.js
@@ -1,24 +1,42 @@
 "use strict";
 
+//Декоратор функции-callback - проверки успешности запроса
+function decoratorCallbackSuccess(funcSuccess, funcError) {
+    function wrapper(response) {
+        if (response.success) {
+            funcSuccess(response);
+        } else if (funcError) {
+            funcError(response);
+        }
+    }
+    return wrapper;
+}
+
+//Декоратор вызова к серверу с использованием данных
+function decoratorApiConnectWithData(methodName, funcCallback) {
+    function wrapper(data) {
+        ApiConnector[methodName](data, funcCallback);
+    }
+    return wrapper;
+}
+
+//действия в случае успешного входа/регистрации
+function reloadPage() {
+    location.reload();
+}
+
+//действия в случае неуспешного входа
+function loginError(response) {
+    currentUserForm.setLoginErrorMessage(response.error);
+}
+
+//действия в случае неуспешной регистрации
+function registerError(response) {
+    currentUserForm.setRegisterErrorMessage(response.error);
+}
+
 let currentUserForm = new UserForm();
-currentUserForm.loginFormCallback = (data) => {
-    ApiConnector.login(data, (response) => {
-        if (response.success) {
-            location.reload();
-        } else {
-            currentUserForm.setLoginErrorMessage(response.error);
-        }
-    });
-
-}
-
-currentUserForm.registerFormCallback = (data) => {
-    ApiConnector.register(data, (response) => {
-        if (response.success) {
-            location.reload();
-        } else {
-            currentUserForm.setRegisterErrorMessage(response.error);
-        }
-    });
-
-}
+currentUserForm.loginFormCallback =
+    decoratorApiConnectWithData('login', decoratorCallbackSuccess(reloadPage, loginError));
+currentUserForm.registerFormCallback =
+    decoratorApiConnectWithData('register', decoratorCallbackSuccess(reloadPage, registerError));
